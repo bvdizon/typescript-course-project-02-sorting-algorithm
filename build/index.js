c9de458284a5a0624a18db13a073822a484e2094
briandizon <bvdizon@gmail.com>
2021-05-01 03:07:54 +0800

    * project complete - with deficiency

--- build/index.js
+++ build/index.js
@@ -1,1 +1,8 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var NumbersCollection_1 = require("./NumbersCollection");
+var Sorter_1 = require("./Sorter");
+var numbersCollection = new NumbersCollection_1.NumbersCollection([10, -5, 18, 3]);
+var sorter = new Sorter_1.Sorter(numbersCollection);
+sorter.sort();
+console.log(numbersCollection);
--- src/Sorter.ts
+++ src/Sorter.ts
@@ -1,17 +1,17 @@
-export default class Sorter {
-  constructor(public collection: /* TODO: fix me later! */) {}
+import { NumbersCollection } from './NumbersCollection';
+
+export class Sorter {
+  constructor(public collection: NumbersCollection) {}
 
   sort(): void {
     // alternative: const lenght = this.collection.length
     const { length } = this.collection;
 
     for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) { 
-        if (this.collection[j] > this.collection[j + 1]) {
-            const leftHand = this.collection[j];
-            this.collection[j] = this.collection[j + 1];
-            this.collection[j + 1] = leftHand;
-        } 
+      for (let j = 0; j < length - i - 1; j++) {
+        if (this.collection.compare(j, j + 1)) {
+          this.collection.swap(j, j + 1);
+        }
       }
     }
   }
