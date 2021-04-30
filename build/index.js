6ca491731392b3dce3162fc90e740c866a85f5fa
briandizon <bvdizon@gmail.com>
2021-05-01 03:35:20 +0800

    lesson 15 - complete

--- build/index.js
+++ build/index.js
@@ -1,8 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var CharactersCollection_1 = require("./CharactersCollection");
 var NumbersCollection_1 = require("./NumbersCollection");
 var Sorter_1 = require("./Sorter");
 var numbersCollection = new NumbersCollection_1.NumbersCollection([10, -5, 18, 3]);
-var sorter = new Sorter_1.Sorter(numbersCollection);
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(numbersCollection);
+var charactersCollection = new CharactersCollection_1.CharactersCollection('BrianVictoriaDizon');
+var sorter = new Sorter_1.Sorter(charactersCollection);
 sorter.sort();
-console.log(numbersCollection);
+console.log(charactersCollection);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
+import { CharactersCollection } from './CharactersCollection';
 import { NumbersCollection } from './NumbersCollection';
 import { Sorter } from './Sorter';
 
 const numbersCollection = new NumbersCollection([10, -5, 18, 3]);
-const sorter = new Sorter(numbersCollection);
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(numbersCollection);
+
+const charactersCollection = new CharactersCollection('BrianVictoriaDizon');
+const sorter = new Sorter(charactersCollection);
 sorter.sort();
-console.log(numbersCollection);
+console.log(charactersCollection);
