d8ecad97034d131013b9fe6ff2233a1725e6e780
briandizon <bvdizon@gmail.com>
2021-05-01 06:18:10 +0800

    lesson 16-18 - complete

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,24 @@
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 import { NumbersCollection } from './NumbersCollection';
 import { Sorter } from './Sorter';
 
-const numbersCollection = new NumbersCollection([10, -5, 18, 3]);
+// const numbersCollection = new NumbersCollection([10, -5, 18, 3]);
 // const sorter = new Sorter(numbersCollection);
 // sorter.sort();
 // console.log(numbersCollection);
 
-const charactersCollection = new CharactersCollection('BrianVictoriaDizon');
-const sorter = new Sorter(charactersCollection);
+// const charactersCollection = new CharactersCollection('BrianVictoriaDizon');
+// const sorter = new Sorter(charactersCollection);
+// sorter.sort();
+// console.log(charactersCollection);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+const sorter = new Sorter(linkedList);
 sorter.sort();
-console.log(charactersCollection);
+linkedList.print();
